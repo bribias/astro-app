283e1c89ed90ab823026d289c83d5b39eb2ba5e8
bribias <bribias@bribiass-MacBook-Pro.local>
2023-05-10 16:17:26 -0700

    setup and fetch on app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [signs, setSigns] = useState([]);
+  const [selectedSign, setSelectedSign] = useState(null);
+
+  useEffect(() => {
+    fetch('http://sandipbgt.com/theastrologer/api/sunsigns/')
+      .then((response) => response.json())
+      .then(setSigns);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Astro App</h1>
+      <h2>Select a sign</h2>
+      {selectedSign}
+      {signs.map((sign) => (
+        <button
+          className="sign"
+          key={sign}
+          onClick={() => setSelectedSign(sign)}
         >
-          Learn React
-        </a>
-      </header>
+          {sign}
+        </button>
+      ))}
     </div>
   );
 }
